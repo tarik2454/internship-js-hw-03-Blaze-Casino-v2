f31a6b87f7b4311da4fb3245ff3b4b0849d7e962
Taras Kovalyk <tarik2454@gmail.com>
2026-01-05 21:07:46 +0200

    feat: enhance project structure by adding Prettier for code formatting, updating font path in layout, and wrapping Home component in a Container. Adjust breakpoints in variables and refine global styles for improved readability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Container } from "../shared/components/Container";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <Container>
+      <div>Home Page</div>
+    </Container>
+  );
 }
