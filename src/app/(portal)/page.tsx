e78a177183da3bddc7afa9949beb906de3887524
Taras Kovalyk <tarik2454@gmail.com>
2026-01-12 20:52:35 +0200

    refactor: remove Leaderboard component and associated styles; update import path in portal page for clarity

--- src/app/(portal)/page.tsx
+++ src/app/(portal)/page.tsx
@@ -1,7 +1,7 @@
 import { ListGames } from "@/module/list-games/ListGames";
 import { Container } from "@/shared/components/Container";
 import { Chat } from "@/module/chat/Chat";
-import { Leaderboard } from "@/module/leaderboard";
+import { Leaderboard } from "@/module/lb";
 import styles from "./page.module.scss";
 import { PageWrapper } from "@/shared/components/PageWrapper";
 
