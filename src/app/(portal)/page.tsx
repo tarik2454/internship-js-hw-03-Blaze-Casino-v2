e5f1a504b7692f23c874c6335757c416e6d9125a
Taras Kovalyk <tarik2454@gmail.com>
2026-01-12 20:45:31 +0200

    fix: update import path for Leaderboard component in portal page to ensure correct module resolution

--- src/app/(portal)/page.tsx
+++ src/app/(portal)/page.tsx
@@ -1,7 +1,7 @@
 import { ListGames } from "@/module/list-games/ListGames";
 import { Container } from "../../shared/components/Container";
 import { Chat } from "@/module/chat/Chat";
-import { Leaderboard } from "@/module/leaderboard/Leaderboard";
+import { Leaderboard } from "../../module/leaderboard/Leaderboard";
 import styles from "./page.module.scss";
 import { PageWrapper } from "@/shared/components/PageWrapper";
 
