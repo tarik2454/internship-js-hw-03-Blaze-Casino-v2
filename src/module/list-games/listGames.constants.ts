61515ce5e1936c28dfea4456d05c1cdcd7f8140b
Taras Kovalyk <tarik2454@gmail.com>
2026-01-13 18:56:43 +0200

    feat: enhance ListGames component with game tags and routing; update styles for improved layout and visual appeal

--- src/module/list-games/listGames.constants.ts
+++ src/module/list-games/listGames.constants.ts
@@ -1,26 +1,43 @@
+import { ROUTES } from "@/shared/constants/routes";
+
+export enum GameTags {
+  TOP = "Top",
+  POPULAR = "Popular",
+  HOT = "Hot",
+  NEW = "New",
+}
+
 export const LIST_GAMES = [
   {
     id: 1,
     name: "Crash",
     description: "Watch the multiplier rise and cash out before it’s gone",
     backgroundImage: "/images/list-games/crash.png",
+    tag: GameTags.TOP,
+    href: ROUTES.CRASH,
   },
   {
     id: 2,
     name: "Case",
     description: "Open cases and win random rewards",
     backgroundImage: "/images/list-games/case.png",
+    tag: GameTags.POPULAR,
+    href: ROUTES.CASE,
   },
   {
     id: 3,
     name: "Mines",
     description: "Avoid the mines and collect bigger rewards",
     backgroundImage: "/images/list-games/mines.png",
+    tag: GameTags.HOT,
+    href: ROUTES.MINES,
   },
   {
     id: 4,
     name: "Plinko",
     description: "Drop the ball, watch it bounce, and win prizes",
     backgroundImage: "/images/list-games/plinko.png",
+    tag: GameTags.NEW,
+    href: ROUTES.PLINKO,
   },
 ];
