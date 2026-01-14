c18b7eb34b175753b0177ad9405338c86e09545d
Taras Kovalyk <tarik2454@gmail.com>
2026-01-14 21:11:38 +0200

    feat: redesign Chat component with enhanced layout and user interaction; implement message input form and user list display

--- src/module/chat/Chat.tsx
+++ src/module/chat/Chat.tsx
@@ -1,7 +1,82 @@
-export function Chat() {
+import Image from "next/image";
+import styles from "./Chat.module.scss";
+import { ArrowTop } from "@/shared/icons/arrow-top";
+
+export async function Chat() {
   return (
-    <div>
-      <div>Chat</div>
-    </div>
+    <section className={styles.chat}>
+      <div className={styles.chatTitleWrapper}>
+        <Image
+          src="/images/chat/chat-title.svg"
+          alt="Chat title"
+          width={100}
+          height={18}
+        />
+      </div>
+
+      <ul className={styles.usersList}>
+        <li className={styles.userItem}>250 online</li>
+        <li className={styles.userItem}>250 online</li>
+        <li className={styles.userItem}>250 online</li>
+      </ul>
+
+      <ul className={styles.chatList}>
+        <li className={styles.chatItem}>
+          <div className={styles.messageHeader}>
+            <Image
+              src="/images/header/user.svg"
+              alt="User avatar"
+              width={44}
+              height={44}
+              className={styles.messageUserAvatar}
+            />
+            <div className={styles.messageUserName}>John Doe</div>
+            <div className={styles.messageTime}>12:00</div>
+          </div>
+          <p className={styles.messageContent}>Hello</p>
+        </li>
+        <li className={styles.chatItem}>
+          <div className={styles.messageHeader}>
+            <Image
+              src="/images/header/user.svg"
+              alt="User avatar"
+              width={44}
+              height={44}
+              className={styles.messageUserAvatar}
+            />
+            <div className={styles.messageUserName}>John Doe</div>
+            <div className={styles.messageTime}>12:00</div>
+          </div>
+          <p className={styles.messageContent}>Hello</p>
+        </li>
+        <li className={styles.chatItem}>
+          <div className={styles.messageHeader}>
+            <Image
+              src="/images/header/user.svg"
+              alt="User avatar"
+              width={44}
+              height={44}
+              className={styles.messageUserAvatar}
+            />
+            <div className={styles.messageUserName}>John Doe</div>
+            <div className={styles.messageTime}>12:00</div>
+          </div>
+          <p className={styles.messageContent}>Hello</p>
+        </li>
+      </ul>
+
+      <form className={styles.chatForm}>
+        <input
+          type="text"
+          id="chat-message"
+          name="message"
+          placeholder="Write a message..."
+          className={styles.chatInput}
+        />
+        <button className={styles.chatButton}>
+          <ArrowTop />
+        </button>
+      </form>
+    </section>
   );
 }
---
+++ src/shared/icons/arrow-top.tsx
@@ -0,0 +1,19 @@
+export function ArrowTop() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m12 5 6 6m-6-6-6 6m6-6v14"
+      />
+    </svg>
+  );
+}
