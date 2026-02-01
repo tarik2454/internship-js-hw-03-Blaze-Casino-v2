3c18bd5a767edd3a887aaf9ff69c08d31a4e5d17
Taras Kovalyk <tarik2454@gmail.com>
2026-02-01 19:45:34 +0200

    fix: update import path for GAMES_LIST constant to ensure correct module resolution

--- src/module/games-list/GamesList.tsx
+++ src/module/games-list/GamesList.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/shared/components/Button";
 import styles from "./GamesList.module.scss";
 import Link from "next/link";
 import { Section } from "@/shared/components/Section";
-import { GAMES_LIST } from "./gamesList.constants";
+import { GAMES_LIST } from "@/module/games-list/gamesList.constants";
 
 export function GamesList() {
   return (
